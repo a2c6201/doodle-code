38482d67aeb86937111904cf96effa8c52b8f59c
a2c6201 <a2c6201@gmail.com>
2024-11-05 23:27:08 +0900

    v-on:inputを学んだ

--- vue-ts-sample/src/components/Payment.vue
+++ vue-ts-sample/src/components/Payment.vue
@@ -1,27 +1,56 @@
 <script setup lang="ts">
-const itemName1 = 'Bike'
+import { ref, reactive } from 'vue'
+
+// const itemName1 = ref<string>('Bike')
 const itemName2 = 'Desk'
 
-const price1 = 40000
+const item1 = reactive({
+  name: 'Desk',
+  price: 40000,
+})
+
+// const price1 = 40000
 const price2 = 20000
 
 const url1 = 'https://www.amazon.co.jp/ref=nav_logo'
-const url2 =
-  'https://www.amazon.co.jp/%E6%95%8F%E6%84%9F%E3%81%99%E3%81%8E%E3%82%8B%E7%A7%81%E3%81%AE%E6%B4%BB%E3%81%8B%E3%81%97%E6%96%B9-%E9%AB%98%E6%84%9F%E5%BA%A6%E3%81%8B%E3%82%89%E6%89%8D%E8%83%BD%E3%82%92%E5%BC%95%E3%81%8D%E5%87%BA%E3%81%99%E7%99%BA%E6%83%B3%E8%A1%93-%E3%82%A8%E3%83%AC%E3%82%A4%E3%83%B3%E3%83%BBN%E3%83%BB%E3%82%A2%E3%83%BC%E3%83%AD%E3%83%B3-ebook/dp/B08F9RFZ3K/?_encoding=UTF8&pd_rd_w=U0sP7&content-id=amzn1.sym.3b2bb6b9-5af8-49d2-a583-fb6c1fc7cca5&pf_rd_p=3b2bb6b9-5af8-49d2-a583-fb6c1fc7cca5&pf_rd_r=S0E6MPC5X71G2EBHNE1D&pd_rd_wg=WowU0&pd_rd_r=53ea29b2-0263-4e37-b2f8-e4b6f12400b2&ref_=pd_hp_d_btf_reads_byrv2_pncv2'
+const url2 = 'https://www.amazon.co.jp/ref=nav_logo'
+
+const buy = (itemName: string) => {
+  alert('Are you sure to buy ' + itemName + '?')
+}
+
+const input = (event: any) => {
+  item1.name = event.target.value
+}
+
+const inputPrice = (event: any) => {
+  item1.price = event.target.value
+}
+
+const clear = () => {
+  item1.name = ''
+  item1.price = 0
+}
 </script>
 
 <template>
   <div class="container">
     <h1>最近の支出</h1>
+    <input v-model="item1.name" />
+    <!-- <input v-on:input="input" v-bind:value="item1.name" /> -->
+    <input v-on:input="inputPrice" v-bind:value="item1.price" />
+    <button v-on:click="clear">Clear</button>
     <div class="payment">
-      <label>{{ itemName1 }}</label>
-      <label>{{ price1 }}</label>
+      <label>{{ item1.name }}</label>
+      <label>{{ item1.price }}</label>
       <a v-bind:href="url1">bought at ...</a>
+      <button v-on:click="buy(item1.name)">Buy</button>
     </div>
     <div class="payment">
       <label>{{ itemName2 }}</label>
       <label>{{ price2 }}</label>
       <a v-bind:href="url1">bought at ...</a>
+      <button v-on:click="buy(itemName2)">Buy</button>
     </div>
   </div>
 </template>
@@ -38,6 +67,10 @@ const url2 =
   justify-content: space-between;
   height: 80px;
   width: 400px;
+  margin-bottom: 8px;
+}
+
+input {
   margin-bottom: 8px;
 }
 
